21f43a518e5f053c6d33c59ecd2eb1895bc7ce01
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-02-26 21:25:45 -0800

    post route - needs to test

--- routes/car-router.js
+++ routes/car-router.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const {Router} = require('express');
+const Car = require('../models/cars.js');
 const jsonParser = require('body-parser').json();
+const carRoutes = module.exports = require('express').Router();
 
-module.exports = new Router;
+carRoutes.post('/api/cars', jsonParser, (req,res,next) => {
+  new Car(req.body).save()
+    .then(res.send.bind(res))
+    .catch(next);
+});
+
