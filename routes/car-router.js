a9f7341518281e89e31ec3b42cec6597de3be31c
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-03-05 20:43:40 -0800

    crud done - now test

--- routes/car-router.js
+++ routes/car-router.js
@@ -1,26 +1,50 @@
 'use strict';
 
-const Car = require('../models/cars.js');
+const httpErrors = require('http-errors');
 const jsonParser = require('body-parser').json();
+
+const Car = require('../models/cars.js');
 const carRoutes = module.exports = require('express').Router();
 
 carRoutes.get('/api/cars', (req,res,next) => {
   Car.find({})
-    .then(res.send.bind(res))
+    .then(cars => res.json(cars))
+    .catch(next);
+});
+
+carRoutes.get('/api/cars/:id', (req,res,next) => {
+  Car.findById(req.params.id)
+    .then(car => {
+      if(!car) return next(httpErrors(404, 'id not found'));
+      res.json(car);
+    })
     .catch(next);
 });
 
 carRoutes.post('/api/cars', jsonParser, (req,res,next) => {
   new Car(req.body).save()
-    .then(res.send.bind(res))
+    .then(car => res.json(car))
     .catch(next);
 });
 
 carRoutes.patch('/api/cars/:id', jsonParser, (req,res,next) => {
   let options = {new:true, runValidators:true};
   Car.findByIdAndUpdate(req.params.id, req.body, options)
-    .then(res.send.bind(res));
-    .catch() //FIGURE OUT WHAT HAPPENS WHEN ID DOESN'T EXIST account for that error in middleware
+    .then(car => {
+      console.log(car);
+      if(!car) return next(httpErrors(404, 'id not found'));
+      res.json(car);
+    })
+    .catch(next);
+});
+
+carRoutes.delete('/api/cars/:id', (req,res,next) => {
+  Car.findByIdAndRemove(req.params.id)
+    .then(car => {
+      if(!car) return next(httpErrors(404, 'id not found'));
+      res.sendStatus(204);
+    })
+    .catch(next);
 });
 
 // delete
@@ -28,3 +52,5 @@ carRoutes.patch('/api/cars/:id', jsonParser, (req,res,next) => {
 // test
 
 // http POST localhost:3000/api/cars < bmw.json
+
+// http patch localhost:3000/api/cars/5a962910bae22a0e271e4b3b year=1969;
